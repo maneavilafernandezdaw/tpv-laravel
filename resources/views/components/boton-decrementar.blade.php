f7064be5a7dd5b305f993e711d61c2c0b151621a
maneavilafernandezdaw <manuelavilafernandez.daw@gmail.com>
2024-02-08 16:07:56 +0100

    iconos bootstrap

--- resources/views/components/boton-decrementar.blade.php
+++ resources/views/components/boton-decrementar.blade.php
@@ -1,4 +1,6 @@
 <button
     {{ $attributes->merge(['type' => 'submit', 'class' => 'btn btn-outline-danger bg-danger-subtle h-10 w-10  text-2xl inline-flex justify-center items-center px-2 py-2  font-semibold  uppercase rounded-md']) }}>
-    <i class="fa-solid fa-minus"></i>
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-lg" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2 8a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11A.5.5 0 0 1 2 8"/>
+      </svg>
 </button>
