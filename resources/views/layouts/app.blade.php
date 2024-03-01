d6634ec76a9dfcaed35a7955f4a2d82da9930a17
maneavilafernandezdaw <manuelavilafernandez.daw@gmail.com>
2024-03-01 09:12:58 +0100

    ult

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,12 +11,14 @@
 
     <title>{{ config('app.name', 'TPV-Laravel') }}</title>
 
+        {{-- script jquery --}}
+        <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     {{-- bootstrap --}}
-    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
 
@@ -26,10 +28,12 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
 
     <!-- Scripts -->
+   
+    {{-- <link href="{{ asset('/bootstrap.css') }}" rel="stylesheet"> --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased m-0 h-0 min-w-fit bg-dark-grey" data-bs-theme ={{ request()->cookie('tema') }}>
+<body class="font-sans antialiased m-0 h-0 min-w-fit bg-dark-grey" data-bs-theme ={{ request()->cookie('tema') }} ">
 
     <div class=" ">
         {{-- nav --}}
@@ -40,14 +44,15 @@
             {{ $slot }}
         </main>
     </div>
+    {{-- script jquery --}}
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     {{-- script bootstrap --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
         integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous">
     </script>
-    {{-- script jquery --}}
-    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+  
     {{-- script datatables --}}
     <script src=" https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src=" https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
