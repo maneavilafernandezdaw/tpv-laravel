561f1493245b8ae2fd8090cdd316320202a1ec3f
maneavilafernandezdaw <manuelavilafernandez.daw@gmail.com>
2024-05-02 11:18:49 +0200

    version sin ajax

---
+++ app/Http/Controllers/ProductosController.php
@@ -0,0 +1,208 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Producto;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Familia;
+use Psy\Readline\Hoa\Console;
+use OpenAI;
+use function Laravel\Prompts\alert;
+
+class ProductosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (Auth::check()) {
+            $productos = Producto::all();
+            $familias = Familia::all();
+
+            try {
+                /* $wmi = new \COM('winmgmts:{impersonationLevel=impersonate}//./root/cimv2');
+                $printers = $wmi->ExecQuery('SELECT * FROM Win32_Printer');
+
+                $impresoras = [];
+                foreach ($printers as $printer) {
+                    $impresoras[] = $printer->Name;
+                } */
+
+                $impresoras= ['tickets', 'cocina'];
+
+                return view('productos.index', compact('productos', 'familias', 'impresoras'));
+            } catch (\Exception $e) {
+
+                return response()->json(['error' => $e->getMessage()], 500);
+            }
+        }
+        return redirect()->route('welcome');
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        if (Auth::check()) {
+            $producto = Producto::find($id);
+
+            return view('productos.show', compact('producto'));
+        }
+        return redirect()->route('welcome');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        if (Auth::check()) {
+            $request->validate([
+                'nombre' => 'required|max:30',
+                'descripcion' => 'max:1500',
+                'precio' => 'required',
+                'iva' => 'required',
+                'familia_id' => 'required',
+                'impresora' => 'required',
+
+            ]);
+
+            $prod = Producto::where('nombre', $request->nombre)->first();
+
+            // Si existe
+            if (!$prod) {
+
+
+                $producto = $request->all();
+
+                if ($request->file('imagen')) {
+                    if ($imagen = $request->file('imagen')) {
+                        $rutaGuardarImg = 'imagen/';
+                        $imagenProducto = date('YmdHis') . "." . $imagen->getClientOriginalExtension();
+                        $imagen->move($rutaGuardarImg, $imagenProducto);
+                        $producto['imagen'] = $imagenProducto;
+                    }
+                }
+
+                Producto::create($producto);
+
+                return redirect()->route('productos.index')
+                    ->with('mensaje', 'Producto creado correctamente.');
+            } else {
+                return redirect()->route('productos.index')
+                    ->with('mensaje', 'Ya Existe un producto con ese nombre.');
+            }
+        }
+        return redirect()->route('welcome');
+    }
+
+    public function coctel(Request $request)
+    {
+        if (Auth::check()) {
+            $request->validate([
+                'nombre' => 'required|max:30',
+                'precio' => 'required',
+                'iva' => 'required',
+
+            ]);
+
+            $prod = Producto::where('nombre', $request->nombre)->first();
+
+            // Si existe
+            if (!$prod) {
+               
+                $client = OpenAI::client("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");
+                
+                $result = $client->chat()->create([
+                    'model' => 'gpt-4',
+                    'messages' => [
+                        ['role' => 'user', 'content' => 'Dime ingredientes, cantidades y como preparar para hacer un cóctel con el nombre de '.$request->nombre],
+                    ],
+                ]);
+                
+           
+
+                $producto = $request->all();
+                $producto['descripcion'] = $result->choices[0]->message->content; 
+
+                Producto::create($producto);
+
+                return redirect()->route('productos.index')
+                    ->with('mensaje', 'Cóctel creado correctamente.');
+            } else {
+                return redirect()->route('productos.index')
+                    ->with('mensaje', 'Ya Existe un cóctel con ese nombre.');
+            }
+        }
+        return redirect()->route('welcome');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,  $id)
+    {
+
+        $prod = Producto::find($id);
+        if (Auth::check()) {
+
+
+
+
+            $producto = $request->all();
+            $producto['impresora'] = $request->impresora;
+
+            if ($request->file('imagen')) {
+
+
+                $rutaGuardarImg = 'imagen/';
+                $imagenProducto = date('YmdHis') . "." . $request->file('imagen')->getClientOriginalExtension();
+                $request->file('imagen')->move($rutaGuardarImg, $imagenProducto);
+                $producto['imagen'] = $imagenProducto;
+
+
+
+                $prod->update($producto);
+
+                return redirect()->route('productos.index')
+                    ->with('mensaje', 'Producto actualizado correctamente.');
+            } else {
+
+                $producto['imagen'] = $prod->imagen;
+                $prod->update($producto);
+
+                return redirect()->route('productos.index')
+                    ->with('mensaje', 'Producto actualizado correctamente.');
+            }
+        } else {
+            return redirect()->route('welcome');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+        if (Auth::check()) {
+            try {
+            $producto = Producto::where('id', $request->idproducto);
+            $producto->delete();
+
+            return redirect()->route('productos.index')
+                ->with('mensaje', 'Producto eliminado correctamente');
+            } catch (\Exception $e) {
+                //return "Error al eliminar producto: " . $e->getMessage();
+                return redirect()->route('productos.index')
+                    ->with('mensaje', 'Este producto no se ha podido eliminar, se está utilizando.');
+            }
+        }
+        return redirect()->route('welcome');
+    }
+}
+
---
+++ app/Http/Controllers/ZonasController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Zona;
+use App\Models\Comanda;
+use Illuminate\Support\Facades\Auth;
+
+use function Laravel\Prompts\text;
+
+use Illuminate\Http\Response;
+
+class ZonasController extends Controller
+{
+
+
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (Auth::check()) {
+            $zonas = Zona::all();
+
+            return view('zonas.index', compact('zonas'));
+        }
+        return redirect()->route('welcome');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+
+        if (Auth::check()) {
+            $zona = Zona::find($id);
+            $comandas = Comanda::all();
+            return view('zonas.show', compact('zona', 'comandas'));
+        }
+        return redirect()->route('welcome');
+    }
+
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        if (Auth::check()) {
+            $request->validate([
+                'nombre' => 'required|max:30',
+                'mesas' => 'required',
+            ]);
+
+            $zon = Zona::where('nombre', $request->nombre)->first();
+
+            // Si existe
+            if (!$zon) {
+                Zona::create($request->all());
+                return redirect()->route('zonas.index')
+                    ->with('mensaje', 'Zona creada correctamente.');
+            } else {
+
+                return redirect()->route('zonas.index')
+                    ->with('mensaje', 'Existe una zona con ese nombre.');
+            }
+        }
+        return redirect()->route('welcome');
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request,  $id)
+    {
+        if (Auth::check()) {
+            $request->validate([
+                'nombre' => 'required|max:30',
+                'mesas' => 'required',
+            ]);
+
+            $zona = Zona::find($id);
+            $zona->update($request->all());
+
+            return redirect()->route('zonas.index')
+                ->with('mensaje', 'Zona actualizada correctamente.');
+        }
+        return redirect()->route('welcome');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+
+        if (Auth::check()) {
+            try {
+                $zona = Zona::where('id', $request->idzona);
+                $zona->delete();
+
+                return redirect()->route('zonas.index')
+                    ->with('mensaje', 'Zona eliminada correctamente');
+            } catch (\Exception $e) {
+                //return "Error al eliminar esta zona: " . $e->getMessage();
+                return redirect()->route('zonas.index')
+                    ->with('mensaje', 'Esta zona no se ha podido eliminar, se está utilizando');
+            }
+        }
+        return redirect()->route('welcome');
+    }
+}
